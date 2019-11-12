aef03358b5d02e5abca03a9fa704df7ead3a7cab
ALau2088 <alau12004@yahoo.com>
2019-11-11 18:38:07 -0800

    added react setup

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add lead manager</h1>
+      </div>
+    )
+  }
+}

5eecbb69a33a83442c9e04f4d48ee3c3ff7a7059
ALau2088 <alau12004@yahoo.com>
2019-12-04 16:59:54 -0800

    implemented Redux

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -39,8 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'leads',
     'rest_framework',
-    'frontend'
+    'frontend',
+    'knox',
+    'accounts'
 ]
+
+REST_FRAMEWORK = {
+  'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',) # tuple needs a trailing comma if no second value or read as string
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
